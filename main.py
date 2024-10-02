9259d684a63e2381a0f42b32c4244622070238ae
Woolzie <snana.swag@gmail.com>
2024-10-02 18:45:35 +0530

    add: included the text in the importance list

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-
-
+'''
+assumptions:- no sudden bold text in middle of text
+'''
 import fitz
 import pymupdf
 import spacy
 import re
 import statistics
-
+from collections import Counter
 nlp = spacy.load("en_core_web_sm")
 
 def extract_text_with_details(file_path):
@@ -26,45 +27,41 @@ def extract_text_with_details(file_path):
     return extracted_data
 
 def findmean(extract_data):
-    size = []
-    weights= []
     importance =[]
     for item in extract_data:
-        # size.append(item["font_size"])
-        # weights.append(item["font_weight"])
-        importance.append(int(item["font_size"])*1.5 + item["font_weight"])
+        font_size = int(item["font_size"]) * 1.5
+        font_weight = item["font_weight"]
+        importance_value = font_size + font_weight
+    
+    # Append as a dictionary
+        importance.append({
+            "importance": importance_value,
+            "text": item["text"]
+            })
+
     return importance
-# use weighted mean to find the answer, for now 2:1, check later
+# use weighted mean to find the answer, for now 1.5:1, check later
 
 
 
                         
 def detect_headings(extracted_data) :
-    headings = []
-    subheadings = []
-    font_list =[]
-    for item in extracted_data:
-        text = item["text"].strip()
-        if not text:
-            continue
-        font_list.append(item["font_size"])
-        # Heuristic based on font size and weight
-        if item["font_size"] > 14 and item["font_weight"] >= 2:  # bold
-            headings.append(text)
-        elif item["font_size"] > 12 and item["font_weight"] >= 2:
-            subheadings.append(text)
-        else:
-            continue
+    # sort by using the values obtained from Counter(importance)
+    return 0
 
-    return headings, subheadings, font_list
+def jsonconvert():
+    return 0
+    
 
 if __name__ ==  "__main__": 
-    files =["phenol-liquid-cert-.pdf","hmm.pdf"]
-    data = extract_text_with_details(files[0])
+    files =["phenol-liquid-cert-.pdf","hmm.pdf", "lorem.pdf"]
+    data = extract_text_with_details(files[2])
     mean = findmean(data)
-    print("big value: "+ str(max(mean)))
-    mode = statistics.mode(mean)
-    print(mode)
+    print(mean)
+    # print("big value: "+ str(max(mean)))
+    # mode = statistics.mode(mean)
+   # print(mode)
+    #print("count: "+ str(Counter(mean)))
 
 ''' style:
 when testing dont use print within the function, return the data and have it print in the main 
